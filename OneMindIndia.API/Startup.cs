654f44b6c8229f541bd1dedceda0d7b8ca559ea8
Nivedha Lakshmanan <nivebiju11@gmail.com>
2020-11-06 17:00:54 +0530

    feat: Added swagger

--- OneMindIndia.API/Startup.cs
+++ OneMindIndia.API/Startup.cs
@@ -4,9 +4,11 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using OneMindIndia.Business.Interface;
 using OneMindIndia.Business.Services;
 using OneMindIndia.DataAccess;
+using System.Linq;
 
 namespace OneMindIndia.API
 {
@@ -27,6 +29,19 @@ namespace OneMindIndia.API
             services.AddControllers();
             services.AddTransient<IProductService, ProductService>();
             services.AddTransient<IOrderService, OrderService>();
+            
+            services.AddSwaggerGen(swagger =>
+            {
+                swagger.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Swagger",
+                    Version = "v1",
+                    Contact = new OpenApiContact
+                    {                        
+                    },
+                });
+                swagger.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,13 +50,20 @@ namespace OneMindIndia.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }
+            }            
 
-            app.UseHttpsRedirection();
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
 
-            app.UseRouting();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.)
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger");
+                c.RoutePrefix = string.Empty;
+            });
 
-            app.UseAuthorization();
+            app.UseRouting();            
 
             app.UseEndpoints(endpoints =>
             {
